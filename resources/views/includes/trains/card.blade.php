64a80145170faae77d215299cf774f5bdc031fff
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-28 16:44:54 +0200

    definitive edition

--- resources/views/includes/trains/card.blade.php
+++ resources/views/includes/trains/card.blade.php
@@ -1,5 +1,9 @@
 <div class="card mb-4">
     <div class="card-body">
-        <h3>{{ $train->agency }}</h3>
+        <h3>Name: {{ $train->agency }}</h3>
+        <h3>Partenza: {{ $train->departure_station }} ore: {{ $train->departure_time }}</h3>
+        <h3>Arrivo: {{ $train->arrival_station }} ore: {{ $train->arrival_time }}</h3>
+        <h3>Codice: {{ $train->train_code }}</h3>
+        <h3>Carrozze: {{ $train->number_coaches }}</h3>
     </div>
 </div>
